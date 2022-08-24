0d73bc0f5230c939668acc32af7307fd02122d09
Omer Minhas <omerminhas1@gmail.com>
2022-08-24 14:40:52 +0100

    Initial Commit

---
+++ src/test/java/com/sparta/om/dto/peopledto/PeopleDTO.java
@@ -0,0 +1,46 @@
+package com.sparta.om.dto.peopledto;
+
+import java.util.List;
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+public class PeopleDTO{
+
+	@JsonProperty("next")
+	private String next;
+
+	@JsonProperty("previous")
+	private Object previous;
+
+	@JsonProperty("count")
+	private Integer count;
+
+	@JsonProperty("results")
+	private List<ResultsItem> results;
+
+	public String getNext(){
+		return next;
+	}
+
+	public Object getPrevious(){
+		return previous;
+	}
+
+	public Integer getCount(){
+		return count;
+	}
+
+	public List<ResultsItem> getResults(){
+		return results;
+	}
+
+	@Override
+ 	public String toString(){
+		return 
+			"PeopleDTO{" + 
+			"next = '" + next + '\'' + 
+			",previous = '" + previous + '\'' + 
+			",count = '" + count + '\'' + 
+			",results = '" + results + '\'' + 
+			"}";
+		}
+}

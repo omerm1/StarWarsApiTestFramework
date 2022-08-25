9f5f5aa1720694622a0a5903e7b94168c2b97929
Omer Minhas <omerminhas1@gmail.com>
2022-08-25 11:25:19 +0100

    Populated people dto tests

--- src/test/java/com/sparta/om/tests/PeopleDTOTests.java
+++ src/test/java/com/sparta/om/tests/PeopleDTOTests.java
@@ -4,6 +4,7 @@ import com.sparta.om.framework.connection.ConnectionManager;
 import com.sparta.om.framework.injection.Injector;
 import com.sparta.om.framework.peopledto.PeopleDTO;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.DisabledIf;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
@@ -47,5 +48,52 @@ public class PeopleDTOTests {
             assertTrue(dto.isCountNotEmpty());
         }
     }
+    @Nested
+    @DisplayName("NextPreviousURLTests")
+    class NextPreviousURLTests {
+        @Test
+        @DisabledIf("onLastPage")
+        @DisplayName("Is next not empty")
+        void isNextNotEmpty(){
+            assertTrue(dto.isNextNotEmpty());
+        }
 
+        @Test
+        @DisabledIf("onLastPage")
+        @DisplayName("Is next not null")
+        void isNextNotNull(){
+            assertTrue(dto.isNextNotNull());
+        }
+
+        @Test
+        @DisplayName("Does next return Url")
+        void doesNextReturnUrl(){
+            assertTrue(dto.DoesNextReturnUrl());
+        }
+
+        @Test
+        @DisplayName("Is previous not empty")
+        void isPreviousNotEmpty(){
+            assertTrue(dto.isPreviousNotEmpty());
+        }
+
+        @Test
+        @DisabledIf("onFirstPage")
+        @DisplayName("Is previous not null")
+        void isPreviousNotNull(){
+            assertTrue(dto.isPreviousNotNull());
+        }
+
+        @Test
+        @DisabledIf("onFirstPage")
+        @DisplayName("Does previous return Url")
+        void doesPreviousReturnUrl(){
+            assertTrue(dto.DoesPreviousReturnUrl());
+        }
+
+        private boolean onFirstPage() {
+            System.out.println(dto);
+            return true;
+        }
+    }
 }
